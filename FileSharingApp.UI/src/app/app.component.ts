b4e8f3e9d92018cb7c3e1b886400ef033452df81
Paul Bailey <52pbailey@gmail.com>
2022-09-22 07:49:28 +0100

    feat: adding initial app-component initialize logic

--- FileSharingApp.UI/src/app/app.component.ts
+++ FileSharingApp.UI/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from './models/user';
+import { AccountService } from './services/account.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'FileSharingApp';
+
+  constructor(
+    private accountService: AccountService
+    ) {}
+
+  ngOnInit(): void {
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user')!);
+
+    if(user) {
+      this.accountService.setLoggedOnUser(user);
+    }
+  }
 }
