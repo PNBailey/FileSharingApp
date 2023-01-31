e9d9b0cc36c3cc767f4a5de094079f9f7000988e
Paul Bailey <52pbailey@gmail.com>
2023-01-31 10:16:23 +0000

    refactor: refactoring MessageHandlingService

--- FileSharingApp.UI/src/app/services/message-handling.service.ts
+++ FileSharingApp.UI/src/app/services/message-handling.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Subject, tap } from 'rxjs';
 import { SnackBarItem } from '../models/snackbar-item';
 
 @Injectable({
@@ -9,6 +10,16 @@ import { SnackBarItem } from '../models/snackbar-item';
 export class MessageHandlingService {
 
   constructor(private snackbar: MatSnackBar) { }
+
+  private displayNewMessage = new Subject<SnackBarItem>();
+
+  newMessageReceived$ = this.displayNewMessage.pipe(
+    tap(snackBarItem => this.displayMessage(snackBarItem))
+  ).subscribe();
+
+  onDisplayNewMessage(snackBarItem: SnackBarItem) {
+    this.displayNewMessage.next(snackBarItem);
+  }
 
   displayMessage(snackBarItem: SnackBarItem) {    
     this.snackbar.open(snackBarItem.message, snackBarItem.action, {
