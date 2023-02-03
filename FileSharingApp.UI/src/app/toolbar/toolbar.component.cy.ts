cf71c6329f558573ad52473ed3b1d694b20bd577
Paul Bailey <52pbailey@gmail.com>
2023-02-03 04:20:50 +0000

    test: adding toolbar component first test

--- FileSharingApp.UI/src/app/toolbar/toolbar.component.cy.ts
+++ FileSharingApp.UI/src/app/toolbar/toolbar.component.cy.ts
@@ -1,40 +1,66 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { HttpClientTestingModule } from '@angular/common/http/testing';
-// import { ToolbarComponent } from './toolbar.component';
-// import { MatDialogModule } from '@angular/material/dialog';
-// import { MatMenuModule } from '@angular/material/menu';
-// import { MatFormFieldModule } from '@angular/material/form-field';
-// import { MatIconModule } from '@angular/material/icon';
-// import { MatToolbarModule } from '@angular/material/toolbar';
-// import { MatInputModule } from '@angular/material/input';
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import { AngularMaterialModule } from '../shared/angular-material.module';
-// import { UntypedFormBuilder } from '@angular/forms';
+import { MatButtonModule } from "@angular/material/button";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatIconModule } from "@angular/material/icon";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { of } from "rxjs";
+import { AccountService } from "../account/account.service";
+import { TestUser } from "../models/test-models/testUser";
+import { ToolbarComponent } from "./toolbar.component";
 
-// describe('ToolbarComponent', () => {
-//   let component: ToolbarComponent;
-//   let fixture: ComponentFixture<ToolbarComponent>;
+describe('ToolBarComponent', () => {
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       providers: [UntypedFormBuilder],
-//       imports: [ 
-//         HttpClientTestingModule,
-//         AngularMaterialModule,
-//         BrowserAnimationsModule
-//       ],
-//       declarations: [ ToolbarComponent ]
-//     })
-//     .compileComponents();
-//   });
+    const user = new TestUser();
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(ToolbarComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    const accountService = {
+        loggedOnUser$: of(user),
+        logout: () => {
+            return null;
+        }
+    }
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+    const elementBindings = {
+        logoutButton: '[data-cy="logout-button"]',
+        userMenuButton: '[data-cy="open-user-menu-button"]',
+    }
+
+    it('mounts', () => {
+        cy.mount(ToolbarComponent, {
+            imports: [
+                MatDialogModule,
+                MatMenuModule,
+                MatToolbarModule,
+                MatButtonModule,
+                BrowserAnimationsModule,
+                MatIconModule
+            ],
+            declarations: [ToolbarComponent],
+            providers: [{provide: AccountService, useValue: accountService}]
+        });
+    });
+    beforeEach(() => {
+        cy.mount(ToolbarComponent, {
+            imports: [
+                MatDialogModule,
+                MatMenuModule,
+                MatToolbarModule,
+                MatButtonModule,
+                BrowserAnimationsModule,
+                MatIconModule
+            ],
+            declarations: [ToolbarComponent],
+            providers: [{provide: AccountService, useValue: accountService}]
+        });
+    });
+    describe('logout method', () => {
+        it('should call the account service logout method', () => {
+            cy.spy(accountService, 'logout').as('accountService-logout-method');
+            cy.get(elementBindings.userMenuButton).click();
+            cy.wait(1000);
+            cy.get(elementBindings.logoutButton).should('be.visible');
+            cy.get(elementBindings.logoutButton).click();
+            cy.get('@accountService-logout-method').should('have.been.called');
+        });
+    });
+});
