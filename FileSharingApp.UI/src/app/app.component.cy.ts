6176f3a86c877a16a213d5623bec297856b34cf5
Paul Bailey <52pbailey@gmail.com>
2023-02-08 02:04:38 +0000

    test: adding new app.component tes

--- FileSharingApp.UI/src/app/app.component.cy.ts
+++ FileSharingApp.UI/src/app/app.component.cy.ts
@@ -1,11 +1,9 @@
-import { createOutputSpy } from "cypress/angular";
 import { AccountService } from "./account/account.service";
 import { AppComponent } from "./app.component";
 import { setupCypressConfig } from "./shared/testing/cypress-config-setup/cypress-config-setup";
 import { getMockAccountService } from "./shared/testing/cypress-config-setup/mock-account-service-setup";
 import { getValidationServiceMock } from "./shared/testing/cypress-config-setup/validation-service-setup";
 import { TestUser } from "./shared/testing/models/testUser";
-import { ToolbarComponent } from "./toolbar/toolbar.component";
 
 describe('AppComponent', () => {
 
@@ -13,10 +11,6 @@ describe('AppComponent', () => {
         sidenavMenu: '[data-cy=sidenav-menu]',
         firstMenuItem: '[data-cy=first-menu-item]',
         toolbar: '[data-cy=toolbar]'
-    }
-
-    const matSidenav = {
-        toggle: () => null
     }
 
     const validationService = getValidationServiceMock();
@@ -38,6 +32,20 @@ describe('AppComponent', () => {
 
     it('should call AccountService setLoggedOnUser method when initialised', () => {
         expect(accountService.setLoggedOnUser).to.be.called;
+    });
+
+    describe('sidenav', () => {
+        it('should open when drawer is toggled', () => {
+            cy.get('mat-sidenav-container').should('have.css', 'width', '500px')
+            .then(($container) => {
+                $container.css({
+                  width: '600px',
+                  height: '600px'
+                });
+              });
+            cy.get(elementBindings.toolbar).trigger('showSideNav', {force:true});
+            cy.get(elementBindings.firstMenuItem).should('be.visible');
+        });
     });
 
     const mountComponent = () => {
