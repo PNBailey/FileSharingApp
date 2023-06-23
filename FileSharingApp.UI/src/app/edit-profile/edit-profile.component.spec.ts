840c4cafbf854689451653000725fca36ce9db26
Paul Bailey <52pbailey@gmail.com>
2023-06-23 07:40:32 +0100

    test: finishing edit-profile jasmine migration

--- FileSharingApp.UI/src/app/edit-profile/edit-profile.component.spec.ts
+++ FileSharingApp.UI/src/app/edit-profile/edit-profile.component.spec.ts
@@ -1,154 +1,4 @@
-// import { of, tap } from "rxjs";
-// import { IdentityResult } from "../models/identityResult";
-// import { SnackbarAction, SnackbarClassType, SnackbarDuration, SnackBarItem } from "../models/snackbar-item";
-// import { MessageHandlingService } from "../services/message-handling.service";
-// import { UserService } from "../services/user.service";
-// import { setupCypressConfig } from "../shared/testing/cypress-config-setup/cypress-config-setup";
-// import { EditProfileComponent } from "./edit-profile.component";
-
-// describe('EditProfileComponent', () => {
-//     it('mounts', () => {
-//         cy.mount(EditProfileComponent, setupCypressConfig<EditProfileComponent>())
-//     });
-
-//     const elementBindings = {
-//         editProfileCardComponent: '[data-cy="edit-profile-card-component"]',
-//         container: '[data-cy="container"]',
-//         updatingProfileSpinner: '[data-cy="updating-profile-spinner"]',
-//         editProfileInfoComponent: '[data-cy="edit-profile-info-component"]'
-//     }
-
-//     const messageHandlingService = {
-//         onDisplayNewMessage: (snackBarItem: SnackBarItem) => {
-//             return snackBarItem;
-//         }
-//     }
-
-//     const userService = {
-//         uploadProfilePicture: (file: File) => {
-//             return of(file);
-//         },
-//         updateUserInfo: () => {
-//             const identityResult = new IdentityResult();
-//             identityResult.succeeded = true;
-//             return of(identityResult).pipe(
-//                 tap((res: IdentityResult) => {
-//                   if(res.succeeded) {
-//                     accountService.setLoggedOnUser();
-//                     messageHandlingService.onDisplayNewMessage({
-//                         message: "Successfully Updated",
-//                         action: SnackbarAction.Close,
-//                         classType: SnackbarClassType.Success,
-//                         duration: SnackbarDuration.Medium
-//                       });
-//                   }
-//                 })
-//             )
-//         }
-//     }
-
-//     const accountService = {
-//         setLoggedOnUser: () => {
-//             return null;
-//         }
-//     };
-
-//     beforeEach(() => {
-//         cy.mount(EditProfileComponent, setupCypressConfig<EditProfileComponent>({
-//             providers: [
-//                 {provide: MessageHandlingService, useValue: messageHandlingService},
-//                 {provide: UserService, useValue: userService}
-//             ]
-//         }));
-//     });
-
-//     it('displayIncorrectFileTypeMessage method should call onDisplayMessage method from the messageHandlingService', () => {
-//         cy.spy(messageHandlingService, 'onDisplayNewMessage').as('messageHandlingService-onDisplayNewMessage-method');
-//         cy.get(elementBindings.editProfileCardComponent).then((editProfileCardComponent) => {
-//             editProfileCardComponent.css({
-//                 width: '600px',
-//                 height: '600px'
-//             });
-//         });
-//         cy.get(elementBindings.editProfileCardComponent).trigger('incorrectFileTypeSelected');
-//         cy.get('@messageHandlingService-onDisplayNewMessage-method').should('have.been.calledOnceWithExactly', {
-//             message: "File selected must be JPEG or PNG file",
-//             action: SnackbarAction.Close,
-//             classType: SnackbarClassType.Error,
-//             duration: SnackbarDuration.Medium
-//         });
-//     });
-
-//     it('uploadProfilePicture method should call user service uploadProfilePicture method', () => {
-//         cy.spy(userService, 'uploadProfilePicture').as('userService-uploadProfilePicture-method');
-//         cy.get(elementBindings.editProfileCardComponent).then((editProfileCardComponent) => {
-//             editProfileCardComponent.css({
-//                 width: '600px',
-//                 height: '600px'
-//             });
-//         });
-//         cy.get(elementBindings.editProfileCardComponent).trigger('newImageSelected');
-//         cy.get('@userService-uploadProfilePicture-method').should('have.been.called');
-//     });
-
-//     it('updating profile loading spinner should be displayed when image is uploading', () => {
-//         cy.mount(EditProfileComponent, setupCypressConfig<EditProfileComponent>({
-//             componentProperties: {
-//                 updatingProfile$: of(true)
-//             }
-//         }));
-//         cy.get(elementBindings.updatingProfileSpinner).should('be.visible');
-//     });
-
-//     it('updating profile loading spinner should not be displayed when profile is not updating', () => {
-//         cy.mount(EditProfileComponent, setupCypressConfig<EditProfileComponent>({
-//             componentProperties: {
-//                 updatingProfile$: of(false)
-//             }
-//         }));
-//         cy.get(elementBindings.updatingProfileSpinner).should('have.length', 0);
-//     });
-
-//     it('UserService updateUserInfo method is called when infoUpdated event is triggered', () => {
-//         cy.spy(userService, 'updateUserInfo').as('userService-updateUserInfo-method');
-//         cy.get(elementBindings.editProfileInfoComponent).then((editProfileInfoComponent) => {
-//             editProfileInfoComponent.css({
-//                 width: '600px',
-//                 height: '600px'
-//             });
-//         });
-//         cy.get(elementBindings.editProfileInfoComponent).trigger('infoUpdated', {force: true});
-//         cy.get('@userService-updateUserInfo-method').should('have.been.called');
-//     });
-
-//     it('AccountService updateUserInfo method is called when infoUpdated event is triggered', () => {
-//         cy.spy(accountService, 'setLoggedOnUser').as('accountService-setLoggedOnUser-method');
-//         cy.get(elementBindings.editProfileInfoComponent).then((editProfileInfoComponent) => {
-//             editProfileInfoComponent.css({
-//                 width: '600px',
-//                 height: '600px'
-//             });
-//         });
-//         cy.get(elementBindings.editProfileInfoComponent).trigger('infoUpdated', {force: true});
-//         cy.get('@accountService-setLoggedOnUser-method').should('have.been.called');
-//     });
-
-//     it('messageHandlingService onDisplayNewMessage method is called when infoUpdated event is triggered', () => {
-//         cy.spy(messageHandlingService, 'onDisplayNewMessage').as('messageHandlingService-onDisplayNewMessage-method');
-//         cy.get(elementBindings.editProfileInfoComponent).then((editProfileInfoComponent) => {
-//             editProfileInfoComponent.css({
-//                 width: '600px',
-//                 height: '600px'
-//             });
-//         });
-//         cy.get(elementBindings.editProfileInfoComponent).trigger('infoUpdated', {force: true});
-//         cy.get('@messageHandlingService-onDisplayNewMessage-method').should('have.been.called');
-//     });
-// });
-
 import { of } from "rxjs";
-import { tap } from "rxjs/operators";
-import { IdentityResult } from "../models/identityResult";
 import { SnackbarAction, SnackbarClassType, SnackbarDuration, SnackBarItem } from "../models/snackbar-item";
 import { MessageHandlingService } from "../services/message-handling.service";
 import { UserService } from "../services/user.service";
@@ -185,41 +35,6 @@ describe('EditProfileComponent', () => {
   const testUser = new User();
   testUser.username = "Mr Test";
   testUser.email = "Test@gmail.com";
-
-  // const messageHandlingServiceMock = {
-  //   onDisplayNewMessage: (snackBarItem: SnackBarItem) => {
-  //     return snackBarItem;
-  //   }
-  // };
-
-  // const accountServiceMock = {
-  //   setLoggedOnUser: () => {
-  //       return null;
-  //   }
-  // };
-
-  // const userServiceMock = {
-  //   uploadProfilePicture: (file: File) => {
-  //     return of(file);
-  //   },
-  //   updateUserInfo: () => {
-  //     const identityResult = new IdentityResult();
-  //     identityResult.succeeded = true;
-  //     return of(identityResult).pipe(
-  //       tap((res: IdentityResult) => {
-  //         if (res.succeeded) {
-  //           accountServiceMock.setLoggedOnUser();
-  //           messageHandlingServiceMock.onDisplayNewMessage({
-  //             message: "Successfully Updated",
-  //             action: SnackbarAction.Close,
-  //             classType: SnackbarClassType.Success,
-  //             duration: SnackbarDuration.Medium
-  //           });
-  //         }
-  //       })
-  //     )
-  //   }
-  // };
 
   beforeEach(() => {
     messageHandlingServiceMock = getMessageHandlingServiceMock();
@@ -266,119 +81,79 @@ describe('EditProfileComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('messageHandlingService.onDisplayNewMessage method should be called when app-edit-profile-card component incorrectFileTypeSelected event is emitted', () => {
-    const snackBarItem: SnackBarItem = {
-      message: "File selected must be JPEG or PNG file",
-      action: SnackbarAction.Close,
-      classType: SnackbarClassType.Error,
-      duration: SnackbarDuration.Medium
-    };
-
-    fixture.debugElement.query(By.css('app-edit-profile-card')).triggerEventHandler('incorrectFileTypeSelected');
-
-    expect(messageHandlingServiceMock.onDisplayNewMessage).toHaveBeenCalledWith(snackBarItem);
-  });
-
-  it('setLoggedOnUser action should be dispatched when app-edit-profile-card component newImageSelected event is emitted', () => {
-    component.loggedOnUser$ = of(testUser);
-    spyOn(store, 'dispatch');
-    const file = new File([], 'dummy.jpg');
-
-    component.uploadProfilePicture(file);
-
-    expect(store.dispatch).toHaveBeenCalledWith(AccountActions.setLoggedOnUser({user: testUser}));
-  });
-
-  it('uploadProfilePicture method should call messageHandlingService.onDisplayNewMessage method when Identity result is succeeded', () => {
-    component.loggedOnUser$ = of(testUser);
-    const file = new File([], 'dummy.jpg');
-    const snackBarItem: SnackBarItem = {
-      message: "Successfully Updated",
-      action: SnackbarAction.Close,
-      classType: SnackbarClassType.Success,
-      duration: SnackbarDuration.Medium
-    };
-
-    component.uploadProfilePicture(file);
-
-    expect(messageHandlingServiceMock.onDisplayNewMessage).toHaveBeenCalledWith(snackBarItem);
-  });
-
-  it('updating profile loading spinner should be displayed when image is uploading', async () => {
-    component.updatingProfile$ = of(true);
-
-    expect(await loader.getHarness(MatProgressSpinnerHarness)).toBeTruthy();
-  });
-
-  it('updating profile loading spinner should not be displayed when image is not uploading', async () => {
-    component.updatingProfile$ = of(false);
-    fixture.detectChanges();
-    expect(await loader.getHarnessOrNull(MatProgressSpinnerHarness.with({selector: '.updating-profile-spinner'}))).toBeNull();
-  });
-
+  describe('incorrectFileTypeSelected event', () => {
+    it('should call messageHandlingService.onDisplayNewMessage method when emitted', () => {
+      const snackBarItem: SnackBarItem = {
+        message: "File selected must be JPEG or PNG file",
+        action: SnackbarAction.Close,
+        classType: SnackbarClassType.Error,
+        duration: SnackbarDuration.Medium
+      };
   
-  it('setLoggedOnUser action should be dispatched when edit profile info component infoUpdated event emitter is emitted and identity result returns succeeded', () => {
-    spyOn(store, 'dispatch');
-    fixture.debugElement.query(By.css('app-edit-profile-info')).triggerEventHandler('infoUpdated', testUser);
-    expect(store.dispatch).toHaveBeenCalledWith(AccountActions.setLoggedOnUser({user: testUser}));
+      fixture.debugElement.query(By.css('app-edit-profile-card')).triggerEventHandler('incorrectFileTypeSelected');
+  
+      expect(messageHandlingServiceMock.onDisplayNewMessage).toHaveBeenCalledWith(snackBarItem);
+    });
   });
 
-  it('messageHandlingService.onDisplayNewMessage method should be called when edit profile info component infoUpdated event emitter is emitted and identity result returns succeeded', () => {
-    const snackBarItem: SnackBarItem = {
-      message: "Successfully Updated",
-      action: SnackbarAction.Close,
-      classType: SnackbarClassType.Success,
-      duration: SnackbarDuration.Medium
-    };
-
-    spyOn(store, 'dispatch');
-    fixture.debugElement.query(By.css('app-edit-profile-info')).triggerEventHandler('infoUpdated', testUser);
-
-    expect(messageHandlingServiceMock.onDisplayNewMessage).toHaveBeenCalledWith(snackBarItem);
+  describe('newImageSelected event', () => {
+    it('should dispatch setLoggedOnUser action when emitted', () => {
+      component.loggedOnUser$ = of(testUser);
+      spyOn(store, 'dispatch');
+      const file = new File([], 'dummy.jpg');
+  
+      component.uploadProfilePicture(file);
+  
+      expect(store.dispatch).toHaveBeenCalledWith(AccountActions.setLoggedOnUser({user: testUser}));
+    }); 
+    it('should call messageHandlingService.onDisplayNewMessage method when Identity result is succeeded', () => {
+      component.loggedOnUser$ = of(testUser);
+      const file = new File([], 'dummy.jpg');
+      const snackBarItem: SnackBarItem = {
+        message: "Successfully Updated",
+        action: SnackbarAction.Close,
+        classType: SnackbarClassType.Success,
+        duration: SnackbarDuration.Medium
+      };
+  
+      component.uploadProfilePicture(file);
+  
+      expect(messageHandlingServiceMock.onDisplayNewMessage).toHaveBeenCalledWith(snackBarItem);
+    });
   });
 
-//   it('updating profile loading spinner should not be displayed when profile is not updating', () => {
-//     component.updatingProfile$ = of(false);
+  describe('updating profile loading spinner', () => {
+    it('should be displayed when image is uploading', async () => {
+      component.updatingProfile$ = of(true);
+  
+      expect(await loader.getHarness(MatProgressSpinnerHarness)).toBeTruthy();
+    });
+    it('should not be displayed when image is not uploading', async () => {
+      component.updatingProfile$ = of(false);
+      fixture.detectChanges();
+      expect(await loader.getHarnessOrNull(MatProgressSpinnerHarness.with({selector: '.updating-profile-spinner'}))).toBeNull();
+    });
+  });
 
-//     const isSpinnerVisible = component.isUpdatingProfileSpinnerVisible();
-
-//     expect(isSpinnerVisible).toBeFalse();
-//   });
-
-//   it('UserService updateUserInfo method is called when infoUpdated event is triggered', () => {
-//     component.infoUpdated();
-
-//     expect(userService.updateUserInfo).toHaveBeenCalled();
-//   });
-
-//   it('AccountService updateUserInfo method is called when infoUpdated event is triggered', () => {
-//     component.infoUpdated();
-
-//     expect(accountService.setLoggedOnUser).toHaveBeenCalled();
-//   });
-
-//   it('messageHandlingService onDisplayNewMessage method is called when infoUpdated event is triggered', () => {
-//     component.infoUpdated();
-
-//     expect(messageHandlingService.onDisplayNewMessage).toHaveBeenCalled();
-//   });
-
-//   it('should call setLoggedOnUser and display success message when updateUserInfo succeeds', () => {
-//     const identityResult = new IdentityResult();
-//     identityResult.succeeded = true;
-
-//     spyOn(identityResult, 'succeeded').and.returnValue(true);
-//     spyOn(userService, 'updateUserInfo').and.returnValue(of(identityResult));
-
-//     component.updateUserInfo();
-
-//     expect(accountService.setLoggedOnUser).toHaveBeenCalled();
-//     expect(messageHandlingService.onDisplayNewMessage).toHaveBeenCalledWith({
-//       message: "Successfully Updated",
-//       action: SnackbarAction.Close,
-//       classType: SnackbarClassType.Success,
-//       duration: SnackbarDuration.Medium
-//     });
-//   });
+  describe('infoUpdated event', () => {
+    it('should dispatch setLoggedOnUser action when emitted and identity result returns succeeded', () => {
+      spyOn(store, 'dispatch');
+      fixture.debugElement.query(By.css('app-edit-profile-info')).triggerEventHandler('infoUpdated', testUser);
+      expect(store.dispatch).toHaveBeenCalledWith(AccountActions.setLoggedOnUser({ user: testUser }));
+    });
+    it('should call messageHandlingService.onDisplayNewMessage method when emitted and identity result returns succeeded', () => {
+      const snackBarItem: SnackBarItem = {
+        message: "Successfully Updated",
+        action: SnackbarAction.Close,
+        classType: SnackbarClassType.Success,
+        duration: SnackbarDuration.Medium
+      };
+  
+      spyOn(store, 'dispatch');
+      fixture.debugElement.query(By.css('app-edit-profile-info')).triggerEventHandler('infoUpdated', testUser);
+  
+      expect(messageHandlingServiceMock.onDisplayNewMessage).toHaveBeenCalledWith(snackBarItem);
+    });
+  })
 
 });
