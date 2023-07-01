25e97f9bce1815e5096f1f07ae96bfc753f9b67e
Paul Bailey <52pbailey@gmail.com>
2023-07-01 18:30:57 +0100

    test: adding sidenav test and deleting redundant test

--- FileSharingApp.UI/src/app/sidenav/sidenav.component.spec.ts
+++ FileSharingApp.UI/src/app/sidenav/sidenav.component.spec.ts
@@ -2,10 +2,14 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SidenavComponent } from './sidenav.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { MatButtonHarness } from '@angular/material/button/testing';
 
 describe('SidenavComponent', () => {
   let component: SidenavComponent;
   let fixture: ComponentFixture<SidenavComponent>;
+  let loader: HarnessLoader;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -17,11 +21,19 @@ describe('SidenavComponent', () => {
     .compileComponents();
 
     fixture = TestBed.createComponent(SidenavComponent);
+    loader = TestbedHarnessEnvironment.loader(fixture);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('my files button should emit goToMyFilesEvent', async () => {
+    spyOn(component.goToMyFilesEvent, 'emit');
+    const myFilesbtn = await loader.getHarness(MatButtonHarness.with({ selector: '.my-files-btn' }));
+    await myFilesbtn.click();
+    expect(component.goToMyFilesEvent.emit).toHaveBeenCalled();
   });
 });
