b9cbe3bd5911fb6199f92bcc1b22853a9082917e
Paul Bailey <52pbailey@gmail.com>
2023-07-05 07:56:29 +0100

    feat: adding initial file upload end point

---
+++ FileSharingApp.API/FileSharingApp.API/Controllers/FileController.cs
@@ -0,0 +1,36 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileSharingApp.API.Controllers
+{
+    public class FileController : BaseController
+    {
+        //[HttpGet]
+        //public IEnumerable<File> Get()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        [HttpPost]
+        public IFormFile Post([FromForm]IFormFile file)
+        {
+            return file;
+        }
+
+        //[HttpPut("{id}")]
+        //public void Put(IFormFile file)
+        //{
+        //}
+
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
