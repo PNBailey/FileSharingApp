2fc5f1b511149bd3da1b5a4d470270ed922258bf
Paul Bailey <52pbailey@gmail.com>
2024-05-06 07:38:44 +0100

    chore: fixing styling

--- FileSharingApp.UI/src/app/home/home.component.ts
+++ FileSharingApp.UI/src/app/home/home.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
+    imports: [
+        MatButtonModule,
+        MatDividerModule
+    ],
     standalone: true
 })
 export class HomeComponent {
