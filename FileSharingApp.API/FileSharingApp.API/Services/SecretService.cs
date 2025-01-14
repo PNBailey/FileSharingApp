4aca1a021a72fdc6bc5f524f726967bf57fb82c9
Paul Bailey <50616601+PNBailey@users.noreply.github.com>
2025-01-14 08:39:34 +0000

    feat: replacing JWT secret with secret manager value (#43)

---
+++ FileSharingApp.API/FileSharingApp.API/Services/SecretService.cs
@@ -0,0 +1,18 @@
+﻿using Google.Cloud.SecretManager.V1;
+
+namespace FileSharingApp.API.Services
+{
+    public static class SecretService
+    {
+        public static AccessSecretVersionResponse GetSecret(AccessSecretVersionRequest request)
+        {
+            SecretManagerServiceClient secretManagerService = SecretManagerServiceClient.Create();
+            AccessSecretVersionResponse response = secretManagerService.AccessSecretVersion(request);
+            if(response == null)
+            {
+                throw new Exception("Secret not found");
+            }
+            return response;
+        }
+    }
+}
